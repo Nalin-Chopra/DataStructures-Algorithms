35a0f9dd57bd6876679ee4c3e8299a77b5a8f738
Nalin Chopra <nalin.chopra@berkeley.edu>
2018-03-10 14:55:54 -0800

    Added BTree class and other data structures & algos solutions

--- DataStructures_Algorithms.py
+++ DataStructures_Algorithms.py
@@ -2,8 +2,6 @@
 and algorithms problems!"""
 
 
-
-""" Yelp Interview """
 def max_subset(arr):
     """Return the value of the maximum subset of an array in terms of
     product. Consider efficiency and shoot for a runtime of 0(N)."""
@@ -18,45 +16,6 @@ def max_subset(arr):
     return max(max_lst)
 
 
-""" Workday Interview """
-
-
-def num_times(x, arr):
-    """Given a sorted array, arr, find the amount of times that integer
-    x appears in a. Optimize for efficiency."""
-    def found_num(x, arr, index):
-        count = 1
-        left_index, right_index = index - 1, index + 1
-        while left_index > -1:
-            if arr[left_index] == x:
-                count += 1
-            else:
-                break
-            left_index -= 1
-        while right_index < len(arr):
-            if arr[right_index] == x:
-                count += 1
-            else:
-                break
-            right_index += 1
-        return count
-
-    def find_nums(x, arr, index=0):
-        if arr[index] != x and len(arr) == 1:
-            return 0
-        elif arr[index] == x:
-            return found_num(x, arr, index)
-        elif arr[index] < x:
-            arr = arr[(index + 1):]
-        else:
-            arr = arr[:index]
-        return find_nums(x, arr, len(arr) // 2)
-
-    return find_nums(x, arr, len(arr) // 2)
-
-
-"""GM AutonomV Final Interview"""
-
 class Cache:
     """Create an API for a LRU or Least-Recently-Used Cache.
     One should be able to assign a certain number of key value pairs to the cache,
@@ -68,7 +27,7 @@ class Cache:
     """
 
     def __init__(self, max_size):
-        self.d = {}
+        self.d = {} #Dictionary with values as nodes of a DLL
         self.head = () #Pointer to Head of Doubly Linked List
         self.tail = () ##Pointer to tail of Doubly Linked List
         self.size = 0
@@ -119,3 +78,219 @@ class Node:
         self.key = key
         self.next = ()
         self.prev = ()
+
+def linked_lst(btree):
+	"""Create a linked_lst function for Binary Trees. The method should return a
+	Linked List that contains all the elemnts of the tree in sorted order."""
+
+	if btree is BTree.empty:
+		return Link.empty
+	rest = Link(t.entry, linked_lst(btree.right))
+	if BTree.left:
+		front = linked_lst(btree.left)
+		result = front
+		while front.rest is not Link.empty:
+			front = front.rest
+		front.rest = rest
+		return result
+	else:
+		return rest
+
+def reverse(lnk):
+    """
+    >>> a = Link(1, Link(2, Link(3)))
+    >>> r = reverse(a)
+    >>> r.first
+    3
+    >>> r.rest.first
+    2
+    """
+# iterative
+    """cur = lnk
+    prev = Link.empty
+    while cur is not Link.empty:
+        nex = cur.rest
+        cur.rest = prev
+        prev = cur
+        cur = nex
+    return prev"""
+
+# reverse reverse recursion
+
+    if lnk is lnk.empty or lnk.rest is lnk.empty:
+        return lnk
+    rest_rev = reverse(lnk.rest)
+    lnk.rest.rest = lnk
+    lnk.rest = Link.empty
+    return rest_rev
+
+
+def widest_level(t):
+    """Return a list of node values of the widest level of a tree.
+
+    >>> sum([[1], [2]], [])
+    [1, 2]
+    >>> t = Tree(3, [Tree(1, [Tree(1), Tree(5)]), Tree(4, [Tree(9, [Tree(2)])])])
+    >>> widest_level(t)
+    [1, 5, 9]
+    """
+    levels = []
+    x = [t]
+    while x:
+        levels.append([t.label for t in x])
+        x = sum([t.branches for t in x], [])
+    return max(levels, key = len)
+
+def num_times(x, arr):
+    """Given a sorted array, arr, find the amount of times that integer
+    x appears in a. Optimize for efficiency."""
+    def found_num(x, arr, index):
+        count = 1
+        left_index, right_index = index - 1, index + 1
+        while left_index > -1:
+            if arr[left_index] == x:
+                count += 1
+            else:
+                break
+            left_index -= 1
+        while right_index < len(arr):
+            if arr[right_index] == x:
+                count += 1
+            else:
+                break
+            right_index += 1
+        return count
+
+    def find_nums(x, arr, index=0):
+        if arr[index] != x and len(arr) == 1:
+            return 0
+        elif arr[index] == x:
+            return found_num(x, arr, index)
+        elif arr[index] < x:
+            arr = arr[(index + 1):]
+        else:
+            arr = arr[:index]
+        return find_nums(x, arr, len(arr) // 2)
+
+    return find_nums(x, arr, len(arr) // 2)
+
+
+def gen_path(t):
+	""" Return a generator for all the paths from a root to leaf.
+	>>> tree = Tree(10, [Tree(1, [Tree(5), Tree(6, [Tree(10)])])])
+	>>> g = gen_path(t)
+	>>> next(g)
+	[10, 1, 5]
+	>>> for path in g:
+		print(path)
+	[10, 1, 6, 10]
+	[10, 10]
+	"""
+	if not t.is_leaf():
+		for paths in [gen_path(b) for b in t.branches]:
+			for path in paths:
+				yield [t.label] + path
+	else:
+		yield [t.label]
+
+
+"""Below are the implementations for the Tree & Linked list data
+structures that I have used throughout the file."""
+
+# Tree Implementation
+class Tree:
+    def __init__(self, label, branches=[]):
+        for c in branches:
+            assert isinstance(c, Tree)
+        self.label = label
+        self.branches = list(branches)
+
+    def __repr__(self):
+        if self.branches:
+            branches_str = ', ' + repr(self.branches)
+        else:
+            branches_str = ''
+        return 'Tree({0}{1})'.format(self.label, branches_str)
+
+    def is_leaf(self):
+        return not self.branches
+
+    def __eq__(self, other):
+        return type(other) is type(self) and self.label == other.label \
+               and self.branches == other.branches
+
+    def __str__(self):
+        def print_tree(t, indent=0):
+            tree_str = '  ' * indent + str(t.label) + "\n"
+            for b in t.branches:
+                tree_str += print_tree(b, indent + 1)
+            return tree_str
+        return print_tree(self).rstrip()
+
+    def copy_tree(self):
+        return Tree(self.label, [b.copy_tree() for b in self.branches])
+
+class BTree:
+    empty = ()
+    def __init__(self, label, left=(), right=()):
+        assert left is BTree.empty or isinstance(left, BTree)
+        assert right is BTree.empty or isinstance(right, BTree)
+        self.label = label
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        if self.left:
+            left_branches = ', ' + repr(self.left)
+        else:
+            left_branches = ''
+        if self.right:
+            right_branches = ', ' + repr(self.right)
+        else:
+            right_branches = ''
+
+        return 'BTree({0}{1}{2})'.format(self.label, left_branches, right_branches)
+
+    def is_leaf(self):
+        return not self.left and not self.right
+
+
+# Link
+class Link:
+    """A linked list.
+
+    >>> s = Link(1, Link(2, Link(3)))
+    >>> s.first
+    1
+    >>> s.rest
+    Link(2, Link(3))
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is Link.empty:
+            return 'Link({})'.format(self.first)
+        else:
+            return 'Link({}, {})'.format(self.first, repr(self.rest))
+
+    def __str__(self):
+        """Returns a human-readable string representation of the Link
+
+        >>> s = Link(1, Link(2, Link(3, Link(4))))
+        >>> str(s)
+        '<1 2 3 4>'
+        >>> str(Link(1))
+        '<1>'
+        >>> str(Link.empty)  # empty tuple
+        '()'
+        """
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
